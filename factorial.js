5ffd8f6ef944ccd070da811db1b98689e3675eb2
raturamadhani01 <raturmn798@gmail.com>
2026-05-04 12:18:56 +0700

    fix: resolve merge conflict in package.json and clean up formatting

--- factorial.js
+++ factorial.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function cariFaktorial(a) {
   // 	your code here
   let result = 1
@@ -10,16 +9,3 @@ function cariFaktorial(a) {
 console.log(cariFaktorial(4))
 
 module.exports = cariFaktorial;
-=======
-function cariFaktorial(a) {
-  // 	your code here
-  let result = 1
-  for(let i = 1; i<= a;i++){
-    result *=i
-  }
-  return result
-}
-console.log(cariFaktorial(4))
-
-module.exports = cariFaktorial;
->>>>>>> 9d4a509 (chore: finis)
--- findSameInteger.js
+++ findSameInteger.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function findSameInteger(a, b) {
   // your code here
   let result = []
@@ -19,25 +18,3 @@ function findSameInteger(a, b) {
 console.log(findSameInteger([1, 2, 3, 5, 9, 12], [1, 2, 5, 10, 12]))
 console.log(findSameInteger([1, 1, 1, 5, 9, 12], [1, 1, 5, 10, 12]))
 module.exports = findSameInteger;
-=======
-function findSameInteger(a, b) {
-  // your code here
-  let result = []
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < b.length; j++) {
-      if (a[i] === b[j] && !result.includes(a[i])) {
-        result.push(a[i])
-      }
-    }
-  }
-  if(result.length === 0){
-    return false
-  }
-
-  return result
-
-}
-console.log(findSameInteger([1, 2, 3, 5, 9, 12], [1, 2, 5, 10, 12]))
-console.log(findSameInteger([1, 1, 1, 5, 9, 12], [1, 1, 5, 10, 12]))
-module.exports = findSameInteger;
->>>>>>> 9d4a509 (chore: finis)
